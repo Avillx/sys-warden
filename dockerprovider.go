4f72772674bafe5a1bdef904765fdb5e87b654ee
Avill <for.sv01@gmail.com>
2026-02-16 18:10:44 +0500

    Docker provider as Command patt

--- dockerprovider.go
+++ dockerprovider.go
@@ -2,34 +2,223 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 )
 
-type DockerRequest interface {
-	endPoint() string
-	method() string
-	query() string
+var DefaultSocketPath = "/var/run/docker.sock"
+var DockerAPIVerison = ""
+
+type ContainerInfoStruct struct {
+	ID   string
+	Name string
+}
+
+type ContainerRecord struct {
+	Id   string
+	Name string
 }
 
 type DockerProvider struct {
 	SocketPath   string
 	SocketClient http.Client
-	DataReader   io.ReadCloser
+	APIVersion   string
 }
 
-func (d *DockerProvider) ExecuteRequest(r DockerRequest) {
+func NewDockerProvider() *DockerProvider {
+	socketPath := DefaultSocketPath
+	socketClient := http.Client{
+		Transport: &http.Transport{
+			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+				return net.Dial("unix", socketPath)
+			},
+		},
+	}
 
-	url := fmt.Sprintf("http://%s/%s", r.endPoint(), r.query())
+	dockerProvider := &DockerProvider{
+		SocketPath:   socketPath,
+		SocketClient: socketClient,
+	}
 
-	socketReqest, err := http.NewRequest(r.method(), url, bytes.NewBuffer([]byte{}))
+	var dockerInfo DockerVersionInfo
+	// execute request without version is unsafe but this counstructor
+	// guarantee that returned provider has version
+	err := dockerProvider.ExecuteDockerRequest(
+		NewGetDockerInfoRequest(&dockerInfo),
+	)
+
+	if err != nil {
+		panic("Docker is unreachible")
+	}
+
+	//TODO:
+	// check version compability
+	DockerAPIVerison = dockerInfo.Components[0].Details.ApiVersion
+	dockerProvider.APIVersion = DockerAPIVerison
+
+	return dockerProvider
+}
+
+func (d *DockerProvider) ExecuteDockerRequest(r Requestable) error {
+
+	ver := ""
+	if r.IsVersionRequired() {
+		ver = fmt.Sprintf("/v%s", d.APIVersion)
+	}
+
+	query := ""
+	if r.Query() != "" {
+		query = fmt.Sprintf("?%s", r.Query())
+	}
+
+	url := fmt.Sprintf("http://localhost%s%s%s", ver, r.EndPoint(), query)
+
+	socketReqest, err := http.NewRequest(r.Method(), url, bytes.NewBuffer([]byte{}))
 
 	if err != nil {
 
-		return _, err
+		return err
 	}
 
-	socketReqest
-	//d.SocketClient.Do()
+	resp, err := d.SocketClient.Do(socketReqest)
+	if err != nil {
+
+		return err
+	}
+
+	return r.Resolve(resp)
+}
+
+type Requestable interface {
+	Query() string
+	Method() string
+	EndPoint() string
+	IsVersionRequired() bool
+	Resolve(resp *http.Response) error
+}
+
+type RequestResolver[T any] func(resp *http.Response, resultPtr *T) error
+
+type DockerRequest[T any] struct {
+	endPoint    string
+	method      string
+	query       string
+	verRequired bool
+	resolver    RequestResolver[T]
+	resultPtr   *T
+}
+
+func (r *DockerRequest[T]) Resolve(resp *http.Response) error {
+	return r.resolver(resp, r.resultPtr)
+}
+
+func (r *DockerRequest[T]) IsVersionRequired() bool { return r.verRequired }
+
+func (r *DockerRequest[T]) EndPoint() string { return r.endPoint }
+
+func (r *DockerRequest[T]) Method() string { return r.method }
+
+func (r *DockerRequest[T]) Query() string { return r.query }
+
+// req
+//
+// req
+type GetDockerInfoRequest struct {
+	DockerRequest[DockerVersionInfo]
+}
+
+func NewGetDockerInfoRequest(result *DockerVersionInfo) *GetDockerInfoRequest {
+	resolverFunc := func(resp *http.Response, resultPtr *DockerVersionInfo) error {
+		data, err := io.ReadAll(resp.Body)
+
+		if err != nil {
+			return err
+		}
+
+		return json.Unmarshal(data, resultPtr)
+	}
+
+	return &GetDockerInfoRequest{
+		DockerRequest: DockerRequest[DockerVersionInfo]{
+			endPoint:    "/version",
+			method:      "GET",
+			query:       "",
+			verRequired: false,
+			resultPtr:   result,
+			resolver:    resolverFunc,
+		},
+	}
+}
+
+// req
+//
+// req
+type GetContainersListRequest struct {
+	DockerRequest[[]DockerContainer]
+}
+
+func NewGetContainersListRequest(result *[]DockerContainer) *GetContainersListRequest {
+
+	resolverFunc := func(resp *http.Response, resultPtr *[]DockerContainer) error {
+		data, err := io.ReadAll(resp.Body)
+
+		if err != nil {
+			return err
+		}
+
+		return json.Unmarshal(data, resultPtr)
+	}
+
+	return &GetContainersListRequest{
+		DockerRequest: DockerRequest[[]DockerContainer]{
+			endPoint:    "/containers/json",
+			method:      "GET",
+			query:       "",
+			verRequired: true,
+			resultPtr:   result,
+			resolver:    resolverFunc,
+		},
+	}
+}
+
+// req
+//
+// req
+type GetContainerOutputRequest struct {
+	DockerRequest[io.ReadCloser]
+}
+
+func NewGetContainerOutputRequest(result *io.ReadCloser, ContainerID string) *GetContainerOutputRequest {
+
+	resolverFunc := func(resp *http.Response, resultPtr *io.ReadCloser) error {
+
+		if resp.StatusCode != 200 {
+			// TODO Exclude to typed errors
+			data, _ := io.ReadAll(resp.Body)
+			errorMessage := fmt.Sprintf("GetContianerOutputerror: %s", string(data))
+			return errors.New(errorMessage)
+		}
+
+		*resultPtr = resp.Body
+		return nil
+	}
+
+	endPoint := fmt.Sprintf("/containers/%s/logs", ContainerID)
+
+	return &GetContainerOutputRequest{
+
+		DockerRequest: DockerRequest[io.ReadCloser]{
+			endPoint:    endPoint,
+			method:      "GET",
+			query:       "stdout=1&stderr=1&follow=1",
+			verRequired: true,
+			resultPtr:   result,
+			resolver:    resolverFunc,
+		},
+	}
 }
--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/binary"
 	"fmt"
+	"io"
+	"time"
 )
 
 func CreateTempoFileObserver() {
@@ -45,8 +48,49 @@ func CreateContainerObserver() {
 	wo.Run(ctx)
 }
 
+func getInfo() {
+	provider := NewDockerProvider()
+
+	var reader io.ReadCloser
+
+	_ = provider.ExecuteDockerRequest(
+		NewGetContainerOutputRequest(&reader, "223ca1b1ff3d1d474edb0938007ed32b9de37d67236b10aff148b0e895bf0b4e"),
+	)
+
+	go func() {
+		for {
+			header := make([]byte, 8)
+
+			_, err := io.ReadFull(reader, header)
+
+			if err != nil {
+				fmt.Printf(err.Error())
+				continue
+			}
+
+			frameSize := binary.BigEndian.Uint32(header[4:])
+			content := make([]byte, frameSize)
+
+			_, err = io.ReadFull(reader, content)
+
+			if err != nil {
+				fmt.Printf(err.Error())
+			}
+
+			if len(content) <= 0 {
+
+				continue
+			}
+
+			fmt.Printf(string(content))
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+}
+
 func main() {
-	CreateContainerObserver()
+	getInfo()
 
 	ctx := context.Background()
 	for _ = range ctx.Done() {
